f9131297fc14649edf04bf8c260f976b8e2539ea
Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>
2020-01-16 22:52:32 -0300

    add
    Signed-off-by: Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+require("dotenv/config");
 const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const routes = require("../src/routes");
-require("dotenv/config");
 
 const app = express();
 mongoose.connect(
