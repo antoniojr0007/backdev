ec2bb29b8bc3b50698bdd2a23668d350e62ff3a9
Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>
2020-01-17 19:21:31 -0300

    update files
    Signed-off-by: Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,6 @@
-const { Router } = require("express");
-const DevController = require("./controllers/DevController")
-const SearchController = require("./controllers/SearchController")
+const { Router } = require('express');
+const DevController = require('./controllers/DevController')
+const SearchController = require('./controllers/SearchController')
 const routes = Router();
 
 // Query: req.query
@@ -11,13 +11,13 @@ const routes = Router();
 routes
 
     // DEVS Endpoint
-    .get("/devs", DevController.index)
-    .post("/devs", DevController.create)
-    .get("/devs/:github", DevController.read)
-    .put("/devs/:github", DevController.update)
-    .delete("/devs/:github", DevController.delete)
+    .get('/devs', DevController.index)
+    .post('/devs', DevController.create)
+    .get('/devs/:github', DevController.read)
+    .put('/devs/:github', DevController.update)
+    .delete('/devs/:github', DevController.delete)
 
     // SEARCH Endpoint
-    .get("/search", SearchController.index);
+    .get('/search', SearchController.index);
 
 module.exports = routes;
