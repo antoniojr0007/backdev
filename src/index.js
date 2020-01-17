8d44e611ab819fd0aeeb0a04d9093b113166192c
Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>
2020-01-16 21:39:09 -0300

    primeiro comite
    Signed-off-by: Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>

---
+++ src/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const routes = require("../src/routes");
+
+const app = express();
+mongoose.connect(
+  "mongodb+srv://antoniojr0007:arucard9@cluster0-kfnmo.mongodb.net/radardev?retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true
+  }
+);
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.listen(3333);
