35b740d69755b0cbd8ad161533e1da69d75c3491
Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>
2020-01-17 17:51:15 -0300

    update files
    Signed-off-by: Antonio Ferreira De Oliveira Junior <antonio-250291@hotmail.com>

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,7 +1,8 @@
+
 const axios = require('axios');
 const Dev = require('../models/Dev');
 const parseStringAsArray = require('../utils/parseStringAsArray');
-const { findConnections, sendMessage } = require('../websocket')
+const { findConnections, sendMessage } = require('../websocket');
 
 module.exports = {
     async index(req, res) {
@@ -34,12 +35,12 @@ module.exports = {
             location,
         });
 
-        const sendSocketMessageTo = findConnections({
-          latitude,
-          longitude },
-          techsArray,
-        )
-        sendMessage(sendSocketMessageTo, 'new-dev', dev)
+        const sendSocketMessageTo = findConnections(
+            { latitude, longitude },
+            techsArray
+        );
+
+        sendMessage(sendSocketMessageTo, 'new-dev', dev);
     
         return res.json(dev);
     },
